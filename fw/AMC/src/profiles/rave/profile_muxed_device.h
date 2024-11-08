48e6be9ae2a31ca967fca9c29db4b6d1588b4a74
Sangshetty Patil <sangshetty.patil@amd.com>
2024-11-08 12:19:20 -0800

    Add Rave profile Basic Rave profile similar to V80 or V70

---
+++ fw/AMC/src/profiles/rave/profile_muxed_device.h
@@ -0,0 +1,17 @@
+/**
+ * Copyright (c) 2024 Advanced Micro Devices, Inc. All rights reserved.
+ * SPDX-License-Identifier: MIT
+ *
+ * This file contains the muxed device profile for Rave
+ *
+ * @file profile_muxed_device.h
+ *
+ */
+
+#ifndef _PROFILE_MUXED_DEVICE_H_
+#define _PROFILE_MUXED_DEVICE_H_
+
+#define MAX_NUM_EXTERNAL_DEVICES_AVAILABLE ( 0 )
+
+
+#endif
