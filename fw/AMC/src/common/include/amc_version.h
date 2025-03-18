d22154e9c5a8409d356d07f3924f4e5f0b011e28
Sangshetty Patil <sangshetty.patil@amd.com>
2025-03-18 04:57:26 +0000

    AMI/AMR version 1.0.0

--- fw/AMC/src/common/include/amc_version.h
+++ fw/AMC/src/common/include/amc_version.h
@@ -10,14 +10,14 @@
 #ifndef _AMC_VERSION_H_
 #define _AMC_VERSION_H_
 
-#define GIT_TAG                   "2.3.0"
-#define GIT_TAG_VER_MAJOR         (2)
-#define GIT_TAG_VER_MINOR         (3)
+#define GIT_TAG                   "1.0.0"
+#define GIT_TAG_VER_MAJOR         (1)
+#define GIT_TAG_VER_MINOR         (0)
 #define GIT_TAG_VER_PATCH         (0)
 #define GIT_TAG_VER_DEV_COMMITS   (0)
 
-#define GIT_HASH                  "37139f62ff1333680cda75c952112a85c01e131d"
-#define GIT_DATE                  "20240725"
+#define GIT_HASH                  "f7169af6c36ba33fbf3caada4ac860f7d0879f68"
+#define GIT_DATE                  "20250317"
 #define GIT_BRANCH                ""
 #define GIT_STATUS                (0)
 
--- sw/AMI/driver/ami_driver_version.h
+++ sw/AMI/driver/ami_driver_version.h
@@ -8,14 +8,14 @@
 #ifndef _AMI_DRIVER_VERSION_H_
 #define _AMI_DRIVER_VERSION_H_
 
-#define GIT_TAG                   "2.3.0"
-#define GIT_TAG_VER_MAJOR         (2)
-#define GIT_TAG_VER_MINOR         (3)
+#define GIT_TAG                   "1.0.0"
+#define GIT_TAG_VER_MAJOR         (1)
+#define GIT_TAG_VER_MINOR         (0)
 #define GIT_TAG_VER_PATCH         (0)
 #define GIT_TAG_VER_DEV_COMMITS   (0)
 
-#define GIT_HASH                  "0bab29e568f64a25f17425c0ffd1c0e89609b6d1"
-#define GIT_DATE                  "20250307"
+#define GIT_HASH                  "f7169af6c36ba33fbf3caada4ac860f7d0879f68"
+#define GIT_DATE                  "20250317"
 #define GIT_BRANCH                ""
 #define GIT_STATUS                (0)
 
